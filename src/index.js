ac2a0864f89579a7f39014dee37f82138bead79c
sofia.belchiorinho.b-i.com <sofia.belchiorinho.b-i.com>
2017-01-27 16:02:22 +0000

    package setup and BtnGreet added

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export default class BtnGreet extends React.Component {  
+
+  render() {
+    return(
+      <div>
+        <button 
+          style="background: gold; border: none; padding: 5px 10px;" 
+          type="button" 
+          onClick={this._greet.bind(this)}>
+          {this.props.buttonText}
+        <button/>
+      </div>
+    );
+  }
+
+  _greet() {
+    alert(this.props.alertMessage);
+  }
+}
+
+BtnGreet.propTypes = {
+	buttonText: React.PropTypes.string.isRequired,
+	alertMessage: React.PropTypes.string.isRequired
+}
+
